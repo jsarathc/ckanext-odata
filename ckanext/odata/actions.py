0d9fc38665aaca7d5be9c6252fec04180159f6e4
Giuseppe Ranieri Musella <ranierimusella2@msn.com>
2022-01-14 17:14:09 +0100

    2To3 performed

--- ckanext/odata/actions.py
+++ ckanext/odata/actions.py
@@ -42,13 +42,13 @@ def name_2_xml_tag(name):
     '''
 
     # leave well-formed XML element characters only
-    name = re.sub(ur'[^:A-Z_a-z\u00C0-\u00D6\u0370-\u037D\u037F-\u1FFF'
-                  ur'\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF'
-                  ur'\uF900-\uFDCF\uFDF0-\uFFFD-.0-9\u00B7\u0300-\u036F'
-                  ur'\u203F-\u2040]', '', name)
+    name = re.sub(r'[^:A-Z_a-z\u00C0-\u00D6\u0370-\u037D\u037F-\u1FFF'
+                  r'\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF'
+                  r'\uF900-\uFDCF\uFDF0-\uFFFD-.0-9\u00B7\u0300-\u036F'
+                  r'\u203F-\u2040]', '', name)
 
     # add '_' in front of non-NameStart characters
-    name = re.sub(re.compile(ur'(?P<q>^[-.0-9\u00B7#\u0300-\u036F\u203F-\u2040])', re.MULTILINE),
+    name = re.sub(re.compile(r'(?P<q>^[-.0-9\u00B7#\u0300-\u036F\u203F-\u2040])', re.MULTILINE),
                   '_\g<q>', name)
 
     # No valid XML element at all
